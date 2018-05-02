c87dcf82cec84e3a1b59508a8baf426965bf0425
Herson <hersoncastillo12@hotmail.com>
2018-05-02 12:02:46 -0600

    -a routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: null },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
