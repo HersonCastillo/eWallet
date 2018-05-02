909d70f4aaf6c04c3cd48fc601989a22d2306e09
Herson <hersoncastillo12@hotmail.com>
2018-05-02 13:05:53 -0600

    -a all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css'
+  ],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {}
