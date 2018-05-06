ca6e0404b43f43acac0c1790c32585b5a83f7851
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-05-05 18:34:27 -0600

    NodeJS 8.11.1 | Angular 6

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('e-wallet App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { MongoService } from './mongo.service';
-import { HttpClientModule } from "@angular/common/http";
-import { RegistrarComponent } from './registrar/registrar.component';
-
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'registrar', component: RegistrarComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login', pathMatch: 'full' }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    RegistrarComponent
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [MongoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
